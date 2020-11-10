4e1d90fe2c6eac6a4c769361f4c3fa88ef61ac85
suyash-dce <jain.suyash28@gmail.com>
2020-11-10 11:36:17 +0530

    Refactor: Code Refactored to Use Function for Returning EmpHours

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,34 +1,24 @@
-//UC-1-Check Employee Absent/Present Using Random
+//UC-3-Using Function to Check Emp Hours
 
-const IS_ABSENT = 0;
-const IS_ABSENT = 0;
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
-let empCheck = Math.floor(Math.random()*10)%2;
-if(empCheck == IS_ABSENT){
-    console.log("Employee is absent");
-    return;
-}
-else {
-    console.log("Employee is present");
+function getWorkingHours(empCheck) {
+    switch(empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
 }
 
-//UC-2-Calculate Daily Wage of Employee
-
-let empHrs = 0;
-switch(empCheck){
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HOURS;
-        break;
-    default:
-        empHrs=0;
-}
+let empCheck = Math.floor(Math.random()*10)%3;
+let empHrs = getWorkingHours(empCheck);
 let empWage = empHrs*WAGE_PER_HOUR;
-console.log("Emp wage = "+empWage)
+console.log("Emp hours: "+empHrs);
+console.log("Emp wage: "+empWage);
